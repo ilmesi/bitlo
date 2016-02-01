80833a88460471cee5eac7a8f7fd3f708dbe5bcb
ilmesi <nachoguason@gmail.com>
2016-02-01 14:28:19 +0100

    more on blockchain info

--- services/blockchain_info.py
+++ services/blockchain_info.py
@@ -2,6 +2,7 @@ import time
 
 import requests
 
+from ..tx import Tx, TxInput, TxOutput
 from .base import ServiceBase
 
 
@@ -9,8 +10,8 @@ class BlockchainInfo(ServiceBase):
     URL = 'https://blockchain.info'
 
     @classmethod
-    def balance(address, confirmations=0):
-        url = self.URL + "/q/addressbalance/{address}"
+    def balance(cls, address, confirmations=0):
+        url = cls.URL + "/q/addressbalance/{address}"
         url = url.format(address=address)
 
         request = requests.get(url, data={
@@ -22,10 +23,10 @@ class BlockchainInfo(ServiceBase):
 
 
     @classmethod
-    def transactions_for_address(address, confirmations):
+    def transactions_for_address(cls, address, confirmations):
         # TODO: handle offsets
 
-        url = self.URL + "/q/rawaddr/{address}"
+        url = cls.URL + "/q/rawaddr/{address}"
         url = url.format(address=address)
 
         request = requests.get(url, data={
@@ -33,4 +34,35 @@ class BlockchainInfo(ServiceBase):
             'nonce': time.time(),
             'api_code': ''
         })
-        response = request.json()
+        raw_txns = request.json()['txs']
+
+        tx = Tx()
+        txns = []
+        inputs = []
+        outputs = []
+        amount = 0
+        for raw_tx in raw_txns:
+
+            for raw_tx_input in raw_tx['inputs']:
+                tx_input = TxInput(
+                    from_address=raw_tx_input['prev_out']['addr'],
+                    amount=raw_tx_input['prev_out']['value']
+                )
+                inputs.push(tx_input)
+
+            for raw_tx_output in raw_tx['out']:
+                tx_output = TxOutput(
+                    to_address=raw_tx_output['addr'],
+                    amount=raw_tx_output['value']
+                )
+                if raw_tx_output['addr'] == address:
+                    amount += raw_tx_output['value']
+                ouputs.push(tx_output)
+
+            tx.inputs = inputs
+            tx.ouputs = ouputs
+            tx.amount = amount
+            tx.tx_hash = raw_tx['hash']
+            txns.push(tx)
+
+        return txns
