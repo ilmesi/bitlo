f57d83836f29bb208cf610f8a956fbf692c06327
ilmesi <nachoguason@gmail.com>
2016-02-01 01:43:28 +0100

    base service

---
+++ services/base.py
@@ -0,0 +1,18 @@
+
+class ServiceBase(object):
+
+    @classmethod
+    def balance(address, confirmations=0):
+        raise NotImplementedError
+
+    @classmethod
+    def transactions_for_address(address, confirmations):
+        raise NotImplementedError
+
+    @classmethod
+    def send(from, to, private, amount_in_satoshis):
+        raise NotImplementedError
+
+    @classmethod
+    def unspents(address, confirmations=0):
+        raise NotImplementedError
